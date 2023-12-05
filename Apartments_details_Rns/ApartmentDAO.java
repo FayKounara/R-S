cbb098021a77410092ed0145c42dbccddc260941
Sintivrousai <116221123+Sintivrousai@users.noreply.github.com>
2023-12-05 16:27:38 +0200

    Add files via upload

---
+++ Apartments_details_Rns/ApartmentDAO.java
@@ -0,0 +1,41 @@
+package Apartments_details_Rns;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import Apartments_details_Rns.Apartment;
+
+public class ApartmentDAO {
+		
+	/**
+	 * This method returns a List with all Users
+	 * 
+	 * @return List<User>
+	 */
+	public List<Apartment> getApartments() throws Exception {
+			
+	} 
+
+	
+	public Apartment findApartment(String user_id) throws Exception {
+		
+	}
+
+	/**
+	 * This method is used to authenticate a user.
+	 * 
+	 * @param username, String
+	 * @param password, String
+	 * @return User, the User object
+	 * @throws Exception, if the credentials are not valid
+	 */
+	public Apartment authenticate(int apartment_id, String name, String available_from, String available_until, 
+                                String city, String address, float price, int capacity, String features, boolean up_rent,boolean up_swap, int user_id) throws Exception {
+		
+			
+			
+		
+	} 
+
+} //End of class
