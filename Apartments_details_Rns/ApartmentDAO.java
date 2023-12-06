38170a772242a7dc577f3e7a21674a24136215f3
Sintivrousai <116221123+Sintivrousai@users.noreply.github.com>
2023-12-06 21:46:41 +0200

    Add files via upload

--- Apartments_details_Rns/ApartmentDAO.java
+++ Apartments_details_Rns/ApartmentDAO.java
@@ -15,12 +15,11 @@ public class ApartmentDAO {
 
 		List<Apartment> apartments = new ArrayList<Apartment>();
 		apartments.add(new Apartment(11, "Maria's apartment", "11/12/2023", "15/12/2023", "Paris","paris", 300, 1,"ok",true, false, 4));
+		apartments.add(new Apartment(12, "Deluxe Apartment", "11/12/2023", "15/12/2023", "Paris","paris", 300, 1,"ok",true, false, 5));
+		apartments.add(new Apartment(13, "Best hotel", "11/12/2023", "15/12/2023", "Athens","paris", 300, 1,"ok",true, false, 6));
 		return apartments;
 			
 	} 
-
-	
-	
 
 	/**
 	 * This method is used to authenticate a user.
@@ -30,18 +29,18 @@ public class ApartmentDAO {
 	 * @return User, the User object
 	 * @throws Exception, if the credentials are not valid
 	 */
-	public Apartment authenticate( String city) throws Exception {  //when it works it need dates and capacity
+	public Apartment authenticate( String city,int cap) throws Exception {  //when it works it need dates and capacity
 		
 		List<Apartment> aparts = getApartments();
+		List<Apartment> available_apartments = new ArrayList<Apartment>();
 
 		for (Apartment apart: aparts) {
 	        //lista kanonika gia na ta krataei ola			
-			if (apart.getCity().equals(city) ) {
-				return apart; 
-			}
-					
+			if (apart.getCity().equals(city) && apart.getCapacity()==cap ) {
+				available_apartments.add(apart); 
+			}				
 		}
-		return null;							
+		return available_apartments;							
 		
 		
 	} 
